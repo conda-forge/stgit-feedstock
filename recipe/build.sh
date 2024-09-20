29bd718e728afe0e0f310c34412e90083253cb45
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-20 14:10:59 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,15 @@ cargo-bundle-licenses \
 
 # build statically linked binary with Rust
 cargo install --no-track --locked --root ${PREFIX} --path .
-mkdir -p ${PREFIX}/etc/bash_completion.d 
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
-mkdir -p ${PREFIX}/share/zsh/site-functions
-stg completion bash > ${PREFIX}/etc/bash_completion.d/stg
-stg completion fish > ${PREFIX}/share/fish/vendor_completions.d/stg.fish
-stg completion zsh > ${PREFIX}/share/zsh/site-functions/_stg
+
+if [[ ${build_platform} == ${target_platform} ]]; then
+    mkdir -p ${PREFIX}/etc/bash_completion.d 
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    stg completion bash > ${PREFIX}/etc/bash_completion.d/stg
+    stg completion fish > ${PREFIX}/share/fish/vendor_completions.d/stg.fish
+    stg completion zsh > ${PREFIX}/share/zsh/site-functions/_stg
+fi
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/stg"
