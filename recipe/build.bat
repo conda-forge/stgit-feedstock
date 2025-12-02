038838cd700e356786842c60bdbf90526f1d8152
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-02 13:27:46 -0800

    devendor openssl

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,12 +1,23 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
+set OPENSSL_DIR=%LIBRARY_PREFIX%
+set OPENSSL_NO_VENDOR=1
 
 :: check licenses
 cargo-bundle-licenses ^
     --format yaml ^
-    --output THIRDPARTY.yml
+    --output THIRDPARTY.yml || goto :error
 
 :: build statically linked binary with Rust
-cargo install --no-track --locked --root %LIBRARY_PREFIX% --path .
+cargo install --no-track --locked --root %LIBRARY_PREFIX% --path . || goto :error
 
-make -C contrib prefix=%LIBRARY_PREFIX% all
+make -C contrib prefix=%LIBRARY_PREFIX% all || goto :error
+
+goto :eof
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
